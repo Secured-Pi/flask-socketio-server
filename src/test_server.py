531b0cbe6fab5cef5b032273df8fa2919215259e
Steven Than <steventhan11@gmail.com>
2016-10-10 13:05:24 -0700

    TDD

---
+++ src/test_server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Testing for Flask-SocketIO server."""
+from server import server
+import unittest
+
+
+class ServerTestCase(unittest.TestCase):
+    """Server test case."""
