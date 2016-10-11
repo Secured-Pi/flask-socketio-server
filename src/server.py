e83e380eafb15e4de440004d3db2ad4b6e890eaf
Steven Than <steventhan11@gmail.com>
2016-10-11 02:48:14 -0700

    incorporate socketio room

--- src/server.py
+++ src/server.py
@@ -2,27 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 
 server = Flask(__name__)
-socketio = SocketIO(server)
+io = SocketIO(server)
 
 
 @server.route('/', methods=['POST'])
 def socketio_channel():
     try:
-        action = request.get_json()['action']
-        socketio.emit(action)
+        data = request.get_json()
+        io.emit(data['action'], room=data['serial'])
         is_sent = True
         message = 'Success'
     except KeyError:
         is_sent = False
-        message = 'No action found, check your json'
+        message = 'Invalid POST data'
     return jsonify({
         'sent': is_sent,
         'message': message,
     })
 
 
+@io.on('listening')
+def join(data):
+    join_room(data['serial'])
+
+
 if __name__ == '__main__':
-    socketio.run(server, port=5000, host='localhost', debug=True)
+    io.run(server, port=5000, host='localhost', debug=True)
